f07f5b6364a45408e5e1450f84f30a42fb258a42
asleirdal <asleirdal@uia.no>
2023-02-12 12:28:03 +0100

    Added base files for project

---
+++ Asak/CMakeFiles/uia-os-boot.dir/src/boot/src/boot.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/uia-os-boot.dir/src/boot/src/boot.cpp.o: \
+ /home/asleirdal/ikt218-osdev/Asak/src/boot/src/boot.cpp
---
+++ Asak/CMakeFiles/uia-os-module-stdlib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uia-os-module-stdlib.dir/src/stdlib/src/system.c.o"
+  "CMakeFiles/uia-os-module-stdlib.dir/src/stdlib/src/system.c.o.d"
+  "libuia-os-module-stdlib.a"
+  "libuia-os-module-stdlib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/uia-os-module-stdlib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
