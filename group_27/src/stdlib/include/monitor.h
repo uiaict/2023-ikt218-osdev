393b49cb911b8edff15f2f6161c0159c6340efba
Nikolai Eidsheim <nikolaie@uia.no>
2023-03-02 08:48:12 +0100

    Done stuff

--- group_27/src/stdlib/include/monitor.h
+++ group_27/src/stdlib/include/monitor.h
@@ -1,18 +1,18 @@
 // monitor.h -- Defines the interface for monitor.h
 // From JamesM's kernel development tutorials.
 
-#ifndef MONITOR_H
-#define MONITOR_H
+#ifndef MONITOR_H
+#define MONITOR_H
 
-#include "common.h"
+#include "common.h"
 
 // Write a single character out to the screen.
-void monitor_put(char c);
+void monitor_put(char c);
 
 // Clear the screen to all black.
-void monitor_clear();
+void monitor_clear();
 
 // Output a null-terminated ASCII string to the monitor.
-void monitor_write(char *c);
+void monitor_write(char *c);
 
-#endif // MONITOR_H
+#endif // MONITOR_H
