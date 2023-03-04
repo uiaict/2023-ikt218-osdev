2099585cb08dfc8036a93b9301e2d2cf71733a65
JonIngvar <joningvarjs@gmail.com>
2023-03-04 17:58:33 +0100

    forsatt

---
+++ group_42/src/kernel/src/main.cpp
@@ -0,0 +1,12 @@
+
+#include "system.h"
+
+// Define entry point in asm to prevent C++ mangling
+extern "C"{
+    void kernel_main();
+}
+
+void kernel_main()
+{
+
+}
--- group_Navn/src/boot/src/boot.cpp
+++
@@ -1,4 +0,0 @@
-loop:
-    jmp loop
-times 510-(#-##) db 0
-dw 0xaa55
