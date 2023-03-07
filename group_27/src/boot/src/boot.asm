7143b50a4434676210bf5ed5ccb3a2747c59b0cd
Nikolai Eidsheim <nikolaie@uia.no>
2023-03-07 13:21:04 +0100

    OS boots, basic printing implemented

---
+++ group_27/src/boot/src/boot.asm
@@ -0,0 +1,25 @@
+MBALIGN  equ  1 << 0
+MEMINFO  equ  1 << 1
+MBFLAGS  equ  MBALIGN | MEMINFO
+MAGIC    equ  0x1BADB002
+CHECKSUM equ -(MAGIC + MBFLAGS)
+section .multiboot
+align 4
+    dd MAGIC
+    dd MBFLAGS
+    dd CHECKSUM
+section .bss
+align 16
+stack_bottom:
+resb 16384 ; 16 KiB
+stack_top:
+section .text
+global _start:function (_start.end - _start)
+_start:
+    mov esp, stack_top
+    extern kernel_main
+    call kernel_main
+    cli
+.hang:    hlt
+    jmp .hang
+.end:
