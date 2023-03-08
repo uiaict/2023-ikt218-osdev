9107cb1ef8283c584b219798449896190d4fba5d
Vegardbeider <vegard@sgjerde.net>
2023-03-08 08:21:00 +0100

    lots of attempts but none working

--- group_7/src/stdlib/src/system.c
+++ group_7/src/stdlib/src/system.c
@@ -1,1 +1,17 @@
 #include "system.h"
+
+/*void *memcpy(void *dest, const void *src, size_t n) 
+{ 
+    // Typecast src and dest addresses to (char *) 
+    char *csrc = (char *)src; 
+    char *cdest = (char *)dest; 
+    
+    // Copy contents of src[] to dest[] 
+    for (size_t i=0; i<n; i++) 
+    {
+        cdest[i] = csrc[i]; 
+
+    }
+    void *nothing;
+    return nothing;
+}*/
