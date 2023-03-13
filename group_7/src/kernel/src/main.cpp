8c5560e33f4093e65e904ea497421a7d657f5cf8
Vegardbeider <vegard@sgjerde.net>
2023-03-13 15:55:44 +0100

    feat: use printf to print Hello World

--- group_7/src/kernel/src/main.cpp
+++ group_7/src/kernel/src/main.cpp
@@ -1,22 +1,13 @@
 
-//#include "system.h"
 
 // Define entry point in asm to prevent C++ mangling
 extern "C"{
+#include "system.h"
     void kernel_main();
 }
 
-void write_string( int colour, const char *string )
-{
-    volatile char *video = (volatile char*)0xB8000;
-    while( *string != 0 )
-    {
-        *video++ = *string++;
-        *video++ = colour;
-    }
-}
 
 void kernel_main()
 {
-    write_string(13, "Halla");
+    printf("Hello World");
 }
