f5e63ae2fec6347444fd886251d1ec40a43d7730
Takelur <91278949+takelur@users.noreply.github.com>
2023-03-15 23:04:31 +0100

    Test monitor_put function in main

--- group_takelur/src/kernel/src/main.cpp
+++ group_takelur/src/kernel/src/main.cpp
@@ -1,11 +1,13 @@
-
-#include "system.h"
-
 // Define entry point in asm to prevent C++ mangling
 extern "C" {
+    #include "common.h"
+    #include "system.h"
+    #include "monitor.h"
     void kernel_main();
 }
 
 void kernel_main() {
-
+    //monitor_clear();
+    //monitor_write((const char*)"Hello, world!"); 
+    monitor_put('K', 0, 15);
 }
