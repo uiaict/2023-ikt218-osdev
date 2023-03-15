22cf00064d11c902d77bb0ff7a998425da8bfec3
Takelur <91278949+takelur@users.noreply.github.com>
2023-03-15 23:03:05 +0100

    Added monitor functions

--- group_takelur/src/kernel/src/monitor.c
+++ group_takelur/src/kernel/src/monitor.c
@@ -23,3 +23,96 @@ static u8int cursor_row = 0;
 
 // Video memory address
 u16int *video_memory = (u16int *)0xB8000;
+
+// Move the hardware cursor
+static void monitor_move_cursor() {
+    u16int location = cursor_row * MONITOR_WIDTH + cursor_column;
+    outb(MONITOR_COMMAND_PORT, MONITOR_HIGH_BYTE_COMMAND);  // Tells VGA board we are setting the cursor row
+    outb(MONITOR_DATA_PORT, location >> 8);          // Sets the row
+    outb(MONITOR_COMMAND_PORT, MONITOR_LOW_BYTE_COMMAND);   // Tells VGA board we are setting the cursor column
+    outb(MONITOR_DATA_PORT, location);               // Send the column
+}
+
+// Scroll text on screen up one line
+static void scroll_monitor() {
+    // Space character with color attribute
+    u8int attribute_byte = (0 << 4) | (15 & 0x0F);          // Black background, white foreground
+    u16int blank = 0x20 | (attribute_byte << 8);            // 0x20 is the space character
+
+    // Scroll up if the cursor has reached the bottom of the screen
+    if (cursor_row >= MONITOR_HEIGHT) {
+        int i;
+        // Moves the whole text on the screen one line up in the video memory
+        for (i = 0 * MONITOR_WIDTH; i < (MONITOR_HEIGHT - 1) * MONITOR_WIDTH; i++) {
+            video_memory[i] = video_memory[i + MONITOR_WIDTH];
+        }
+
+        // Then write blank characters to the last line
+        for (i = (MONITOR_HEIGHT - 1) * MONITOR_WIDTH; i < MONITOR_HEIGHT * MONITOR_WIDTH; i++) {
+            video_memory[i] = blank;
+        }
+        
+        // Finally set the cursor to the last line
+        cursor_row = MONITOR_HEIGHT - 1;
+}
+
+// Writes a byte to the command port
+void monitor_put(u8int c, u8int bg_color, u8int fg_color) {
+    // Get the color byte (high nibble is background color and low nibble is foreground color)
+    u8int color = (bg_color << 4) | (fg_color & 0x0F);
+
+    // Get the attribute byte, which is the top 8 bits of the word we write to the video memory
+    u16int attribute = color << 8;
+
+    u16int *location; // Pointer to the video memory location
+
+    // Backspace moves cursor to the previous column
+    if (c == '\b') {
+        if (cursor_column > 0) {
+            cursor_column--;
+        }
+        // Go to previous row if we are at the first column
+        else if (cursor_row > 0) {
+            cursor_column = MONITOR_WIDTH - 1;
+            cursor_row--;
+        }
+    }
+    // Newline moves cursor to the first column of the next row
+    else if (c == '\n') {
+        cursor_column = 0;
+        cursor_row++;
+    }
+    // Carriage return moves cursor to the first column
+    else if (c == '\r') {
+        cursor_column = 0;
+    }
+    // Tab moves cursor to next tab stop (increasing column until it is a multiple of 8)
+    else if (c == '\t') {
+        if (cursor_column < MONITOR_WIDTH - 8) {
+            cursor_column = (cursor_column + 8) & ~(8 - 1);
+        }
+        else {  // Set to last column if there is no more room
+            cursor_column = MONITOR_WIDTH - 1;
+        }
+    }
+    // Any other character is written to the video memory
+    else if (c >= ' ') {
+        location = video_memory + (cursor_row * MONITOR_WIDTH + cursor_column);
+        *location = c | attribute;      // This sets the character as the lower byte of "attribute"
+        cursor_column++;
+    }
+
+    // Safety check in case we have reached the end
+    if (cursor_column >= MONITOR_WIDTH) {
+        cursor_column = 0;
+        cursor_row++;
+    }
+
+
+/*
+// Clear the screen to all black.
+void monitor_clear();
+
+// Output a null-terminated ASCII string to the monitor.
+void monitor_write(char *c);
+*/
