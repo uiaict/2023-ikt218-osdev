13766d609c5cd9ed0436ebcc355b7bb65e00a6a2
Jon <jona.larss@gmail.com>
2023-03-23 18:06:41 +0000

    Add gdt.h, gdt.cpp, gdt.asm and updated CMakeLists

---
+++ group_OSDev5/src/kernel/cpu/gdt.h
@@ -0,0 +1,21 @@
+#include <stdint.h>
+
+namespace UIAOS::CPU::GDT {
+  struct gdt_entry {
+    uint16_t limit_low;
+    uint16_t base_low;
+    uint8_t base_middle;
+    uint8_t access;
+    uint8_t granularity;
+    uint8_t base_high;
+  } __attribute__((packed));
+
+  struct gdt_ptr {
+    uint16_t limit;
+    uint32_t base;
+  } __attribute__((packed));
+
+  void gdt_init();
+  void gdt_setGate(int32_t num, uint32_t base, uint32_t limit, uint8_t access, uint8_t gran);
+} 
+
