7bc992e6a72b461da409b257162b24c3b7582005
Jon <jona.larss@gmail.com>
2023-03-23 18:49:14 +0000

    Add functionality for printing to screen

--- group_OSDev5/src/kernel/src/main.cpp
+++ group_OSDev5/src/kernel/src/main.cpp
@@ -1,12 +1,31 @@
-
-#include "system.h"
+#include <system.h>
 
 // Define entry point in asm to prevent C++ mangling
 extern "C"{
     void kernel_main();
 }
+void clear_screen() {
+    char* vidmem = (char*)0xb8000;
+    for (int i = 0; i < 80 * 25 * 2; i += 2) {
+        vidmem[i] = ' ';
+        vidmem[i+1] = 0x0f; // black background, white foreground
+    }
+}
+
+void print(const char* str) {
+    char* vidmem = (char*)0xb8000;
+    int i = 0;
+    while (str[i] != '\0') {
+        vidmem[i * 2] = str[i];
+        vidmem[i * 2 + 1] = 0x0f; // Set color attribute to white on blue
+        i++;
+    }
+}
 
 void kernel_main()
 {
+
+    clear_screen();
+    print("Hello, world");
 
 }
