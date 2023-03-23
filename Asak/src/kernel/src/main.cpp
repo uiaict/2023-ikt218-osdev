3f0e88c7bf3ccaaa0970f4471efe7e5d4cf6851f
TheIsaaak <isakfage@gmail.com>
2023-03-23 04:15:32 +0100

    Add stdlib, but without a working CMakeLists reffrence

--- Asak/src/kernel/src/main.cpp
+++ Asak/src/kernel/src/main.cpp
@@ -1,11 +1,14 @@
-#include <system.h>
 
 // Define entry point in asm to prevent C++ mangling
 extern "C"{
+    #include "system.h"
     void kernel_main();
 }
 
 void kernel_main()
 {
-
+    int fem = 5;
+    char* ord = "helloworld!";
+    size_t lengde = strlen(ord);
+    int seks = 6;
 }
--- Asak/src/stdlib/src/system.c
+++ Asak/src/stdlib/src/system.c
@@ -1,1 +1,11 @@
 #include "system.h"
+
+size_t strlen(const char *s)
+{
+    int length = 0;
+    while (s[length])
+    {
+        length++;
+    }
+    return length;
+}
