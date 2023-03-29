5ed20c92d297a3fde692538a1cf095b25e2e4167
shellby <clangs-k@hotmail.com>
2023-03-29 18:48:45 +0200

    Implemted GDT and print function

--- group_14/src/kernel/src/main.cpp
+++ group_14/src/kernel/src/main.cpp
@@ -1,12 +1,25 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 
-#include "system.h"
+// Custom files
+#include "include/descriptor_tables.h"
+
 
 // Define entry point in asm to prevent C++ mangling
 extern "C"{
-    void kernel_main();
+   void kernel_main();
 }
 
 void kernel_main()
 {
+	// Initialize Global Descriptor Table
+	init_gdt();
 
+	// Initialize terminal and
+	// and display a string.
+    terminal_initialize();
+    terminal_writestring("Hello, World!");
 }
+
+
