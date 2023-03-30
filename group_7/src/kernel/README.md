94e695970ec62871a07e76d48bd4847f66afbc45
Daniel Sandøy <daniev18@uia.no>
2023-03-30 15:39:15 +0200

    added idt documentation

--- group_7/src/kernel/README.md
+++ group_7/src/kernel/README.md
@@ -1,10 +1,18 @@
-# Global Description Table
+# 🧠 Kernel
+This module of the os contains the source code for the kernel and tables the kernel rely upon
+to function. The different tables are described below. For a thourough documentation of the 
+source code, see the code files, as all functions are documented and explained there. This
+readme presents a short overview of the concepts that have been implemented in the kernel module.
+
+
+## 🌐 Global Description Table
 The files gdt.h and gdt.c contains the source code for implementing the global description table.
 Most of the source code in this folder has been borrowed from the lecture notes "Lecture 4 - Global description tables" by Per-Arne.
 
-## Description
-### Definition of global description table.
 
-### Layout of the description table
+## 🆘 Interupt Descriptor table
+IDT is a binary data structure that tells the CPU where the interupt service routines(ISR) are located. A global desciption table is required in order for the IDT to work.
+<!-- source: https://wiki.osdev.org/Interrupt_Descriptor_Table -->
 
-## Use
+The files idt.h, idt.cpp and idt.asm implement the IDT. In order to have a functional IDT, some 
+interupt service routines needs to be defined as well. This is done in the interupt.h, interupt.cpp, isr.cpp and isr.asm files.
--- group_7/src/kernel/include/idt.h
+++ group_7/src/kernel/include/idt.h
@@ -1,35 +1,61 @@
 #ifndef IDT_H
 #define IDT_H
+/*
+This header file contains the definitions of the functions that create 
+an interupt descriptor table (idt) in our os. The table defines
+interupt numbers and points to interupt handling functions that deals
+with the issue or reason for the interupt.
 
+The documentation of this file is done with help of the IDt article at osDev
+https://wiki.osdev.org/Interrupt_Descriptor_Table
+
+*/
 #include <stdint.h>
 
-#define IDT_ENTRIES 256
+#define IDT_ENTRIES 256 // this os may have a total of 256 different interupts.
 
-// Define the IDT entry structure
+/**
+ * @brief Defines the structure of an idt entry.
+ * 
+ */
 struct idt_entry_t {
+  /// @brief offset bits 0..15 of the base address.
   uint16_t base_low;
+  /// @brief a code segment selector. Point to a field of executable code.
   uint16_t selector;
+  /// @brief A reserved field that must be 0x00
   uint8_t zero;
+  /// @brief  the gate type, dpl (CPU privelige level) and p (present bit) fields
   uint8_t flags;
+  /// @brief offset bits 16...31 of the base address..
   uint16_t base_high;
-} __attribute__((packed));
+} __attribute__((packed)); // reduces the size of the struct in memory.
 
-
+/**
+ * @brief A pointer to the idt table.
+ * The base is the lowest address, and the start of the table
+ * The limit is the highest address of the idt.
+ */
 struct idt_ptr_t {
-  uint16_t limit;
-  uint32_t base;
-} __attribute__((packed));
+  /// the highest accessable address in the idt. I.e. the last idt entry.
+  uint16_t limit; 
+  /// the first accessable address in the idt.
+  uint32_t base; 
+} __attribute__((packed)); // reduces the size of the struct in memory.
 
-
-// Initialize the GDT and IDT
+/**
+ * @brief Creates and loads the idt table and its interupts.
+ * First, it creates the idt_ptr, then fills the table 
+ * with idt entries, inserts the data into the 
+ * interupt handler (see interupts.h), and then loads
+ * the idt using asm.
+ */
 void init_idt();
 
-// Load the GDT and IDT
-void idt_load();
 
-
-
+/// @brief The idt table of 256 entries.
 static idt_entry_t idt[IDT_ENTRIES];
+/// @brief pointer to the idt table and variable.
 static idt_ptr_t idt_ptr;
 
 #endif
