cb5c05081a029bcebbe16af818ff85ca7efd43c1
tuva <tuvaea@uia.no>
2023-03-30 11:38:20 +0200

    gdt

--- group_tuva/src/kernel/src/main.cpp
+++ group_tuva/src/kernel/src/main.cpp
@@ -1,12 +1,21 @@
-
 #include "system.h"
+#include "gdt.h"
+#include "common.h"
 
 // Define entry point in asm to prevent C++ mangling
 extern "C"{
     void kernel_main();
+    void init_descriptor_tables();
 }
 
 void kernel_main()
 {
+    printf("Hello");
+    init_descriptor_tables();
+    printf("gdt initialized");
 
+    
 }
+
+
+
--- group_tuva/src/stdlib/src/system.c
+++ group_tuva/src/stdlib/src/system.c
@@ -1,1 +1,15 @@
 #include "system.h"
+#include "stdbool.h"
+
+int printf(const char *string, ...)
+    {
+        volatile char *video = (volatile char*)0xB8000;
+        while( *string != 0 )
+        {
+            *video++ = *string++;
+            *video++ = 0x0F;
+        }
+    }
+
+
+
