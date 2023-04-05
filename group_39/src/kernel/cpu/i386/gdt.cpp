906c6bfe4be3681d456d8f4904cbe61711df442e
Daniel Dybing <danieldy@uia.no>
2023-04-06 01:02:13 +0200

    Started implementation of GDT

--- group_39/src/kernel/cpu/i386/gdt.cpp
+++ group_39/src/kernel/cpu/i386/gdt.cpp
@@ -1,2 +1,11 @@
-#include "gdt.h"
+// Written by the IKT218 Group 39 in the spring 2023.
 
+#include "gdt.h" // Include our GDT header file.
+
+
+
+
+// Create an array of 3 GDT entries (NULL, Data and Text) 
+// as well as a GDT pointer on stack.
+gdt_entry gdt_entries[3];
+gdt_pointer gdt_ptr; 
