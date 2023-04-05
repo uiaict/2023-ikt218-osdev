8cede4477375158e234623441685d92253ad1611
Daniel Dybing <danieldy@uia.no>
2023-04-06 00:22:02 +0200

    Changed some comments

--- group_39/src/kernel/cpu/i386/gdt.h
+++ group_39/src/kernel/cpu/i386/gdt.h
@@ -1,47 +1,26 @@
 // 
-// Writtten by IKT 218 Group 39 in the spring 2023. 
-
-
-// stdint.h is a freestanding libc header file and can be used in our OS, even though we do not have POSIX support.
-#include <stdint.h> 
-
-
+// Writtten by IKT218 Group 39 in the spring 2023. 
 
 // This is the header file for our Global Descriptor Table (GDT) implementation, which we will
 // use to set up our memory segments. This will later allow us to enter Protected mode. 
 
 
-
-// Here we define a struct which represents a GDT entry, called a segment descriptor.
-
-
-// Our struct contains 6 fields
-
-// **Limit low**: The lower 16 bits of the segment limit. 
-
-// **Base Low**: The lower 16 bits of the base address. There are a total of 
-// three base address fields because of backward compatibility with 16-bit processors. 
-
-// **Base Middle**: The middle 8 bits of the base address
-
-
-//// Base High: The last 8 bits of the base address.
+// stdint.h is a freestanding libc header file and can be used in our OS, even though we do not have POSIX support.
+// We use some of the types that it define, like uint16_t - which is a fixed width data types. 
+#include <stdint.h> 
 
 
 
-// The __attribute__((packed)) is used to tell the compiler
-// to not pack the struct. When a struct is packed, the compiler 
-// may add padding to the struct to make it more memory efficient.
-// But this may alter the size of the struct, causing memory alignment issues, which is 
-// very much unwanted.
 
 
+
+// Our GDT entry (segment descriptor) struct. 
 struct gdt_entry {
-    uint16_t limit_low;
-    uint16_t base_low;
-    uint8_t base_middle;
+    uint16_t limit_low; // The lower 16 bits of the segment limit
+    uint16_t base_low; // The lower 16 bits of the base address. 
+    uint8_t base_middle; // The middle 8 bits of the base address. 
     uint8_t access;
     uint8_t granularity;
-    uint8_t base_high;
-} __attribute__((packed));
+    uint8_t base_high; // The upper 8 bits of the base address.
+} __attribute__((packed)); // This attribute ells the compiler to not add padding to the struct, which would increase its size. 
 
