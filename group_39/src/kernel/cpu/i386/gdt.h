e1edb43a74fc8a19b65edc96a1ac7866fd77deca
Daniel Dybing <danieldy@uia.no>
2023-04-06 00:44:34 +0200

    Added include guards to GDT header

--- group_39/src/kernel/cpu/i386/gdt.h
+++ group_39/src/kernel/cpu/i386/gdt.h
@@ -1,8 +1,16 @@
 // 
-// Writtten by IKT218 Group 39 in the spring 2023. 
+// Written by IKT218 Group 39 in the spring 2023. 
+
 
 // This is the header file for our Global Descriptor Table (GDT) implementation, which we will
 // use to set up our memory segments. This will later allow us to enter Protected mode. 
+
+
+
+// Adding ifndef to avoid include errors.
+#ifndef UIAOS_GDT_H
+#define UIAOS_GDT_H
+
 
 
 // stdint.h is a freestanding libc header file and can be used in our OS, even though we do not have POSIX support.
@@ -17,14 +25,17 @@ struct gdt_entry {
     uint16_t limit_low; // The lower 16 bits of the segment limit
     uint16_t base_low; // The lower 16 bits of the base address. 
     uint8_t base_middle; // The middle 8 bits of the base address. 
-    uint8_t access;
-    uint8_t granularity;
+    uint8_t access; // NEED COMMENT HERE
+    uint8_t granularity; // NEED COMMENT HERE
     uint8_t base_high; // The upper 8 bits of the base address.
 } __attribute__((packed)); // This attribute ells the compiler to not add padding to the struct, which would increase its size. 
 
 
 // The GDT pointer struct which will be contained in the GDT register.
 struct gdt_ptr {
-    uint16_t limit; // 
-    uint32_t base;
+    uint16_t limit; // NEED COMMENT HERE
+    uint32_t base; // NEED COMMENT HERE
 } __attribute__((packed)); // As above, tell the compiler not to add padding.
+
+
+#endif UIAOS_GDT_H
