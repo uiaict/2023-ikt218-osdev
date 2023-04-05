a94f20f19e2303850b52dbab629faa965d5ebd3f
Espen Klungland <espenk13@uia.no>
2023-04-05 09:06:31 +0200

    Added ISR

--- group_espen_k/src/kernel/cpu/idt.cpp
+++ group_espen_k/src/kernel/cpu/idt.cpp
@@ -8,8 +8,7 @@ extern "C" {
   extern void idt_flush(uint32_t);
 }
 
-// Array to hold information about registered interrupt handlers
-struct ESOS::IDT::int_handler_t int_handlers[IDT_ENTRIES];
+
 struct ESOS::IDT::idt_entry_t idt[IDT_ENTRIES];
 struct ESOS::IDT::idt_ptr_t idt_ptr;
 
@@ -29,10 +28,57 @@ void ESOS::IDT::init_idt() {
     idt[i].zero = 0x00;
     idt[i].flags = 0x8E;
 
-	int_handlers[i].handler = NULL;
+	  ESOS::IDT::int_handlers[i].handler = NULL;
   }
+  
+  ESOS::IDT::init_interrupts();
+  ESOS::IDT::init_interrupt_handlers();
 
   // Load the IDT
   idt_flush((uint32_t)&idt_ptr);
 
+}
+
+void ESOS::IDT::idt_set_gate(uint8_t num, uint32_t base, uint16_t sel, uint8_t flags)
+{
+  idt[num].base_low = base & 0xFFFF;
+  idt[num].base_high = (base >> 16) & 0xFFFF;
+  idt[num].selector = sel;
+  idt[num].zero = 0;
+  idt[num].flags = flags  | 0x60;
+}
+
+void ESOS::IDT::init_interrupts(){
+  idt_set_gate( 0, (uint32_t)isr0 , 0x08, 0x8E);
+  idt_set_gate( 1, (uint32_t)isr1 , 0x08, 0x8E);
+  idt_set_gate( 2, (uint32_t)isr2 , 0x08, 0x8E);
+  idt_set_gate( 3, (uint32_t)isr3 , 0x08, 0x8E);
+  idt_set_gate( 4, (uint32_t)isr4 , 0x08, 0x8E);
+  idt_set_gate( 5, (uint32_t)isr5 , 0x08, 0x8E);
+  idt_set_gate( 6, (uint32_t)isr6 , 0x08, 0x8E);
+  idt_set_gate( 7, (uint32_t)isr7 , 0x08, 0x8E);
+  idt_set_gate( 8, (uint32_t)isr8 , 0x08, 0x8E);
+  idt_set_gate( 9, (uint32_t)isr9 , 0x08, 0x8E);
+  idt_set_gate(10, (uint32_t)isr10, 0x08, 0x8E);
+  idt_set_gate(11, (uint32_t)isr11, 0x08, 0x8E);
+  idt_set_gate(12, (uint32_t)isr12, 0x08, 0x8E);
+  idt_set_gate(13, (uint32_t)isr13, 0x08, 0x8E);
+  idt_set_gate(14, (uint32_t)isr14, 0x08, 0x8E);
+  idt_set_gate(15, (uint32_t)isr15, 0x08, 0x8E);
+  idt_set_gate(16, (uint32_t)isr16, 0x08, 0x8E);
+  idt_set_gate(17, (uint32_t)isr17, 0x08, 0x8E);
+  idt_set_gate(18, (uint32_t)isr18, 0x08, 0x8E);
+  idt_set_gate(19, (uint32_t)isr19, 0x08, 0x8E);
+  idt_set_gate(20, (uint32_t)isr20, 0x08, 0x8E);
+  idt_set_gate(21, (uint32_t)isr21, 0x08, 0x8E);
+  idt_set_gate(22, (uint32_t)isr22, 0x08, 0x8E);
+  idt_set_gate(23, (uint32_t)isr23, 0x08, 0x8E);
+  idt_set_gate(24, (uint32_t)isr24, 0x08, 0x8E);
+  idt_set_gate(25, (uint32_t)isr25, 0x08, 0x8E);
+  idt_set_gate(26, (uint32_t)isr26, 0x08, 0x8E);
+  idt_set_gate(27, (uint32_t)isr27, 0x08, 0x8E);
+  idt_set_gate(28, (uint32_t)isr28, 0x08, 0x8E);
+  idt_set_gate(29, (uint32_t)isr29, 0x08, 0x8E);
+  idt_set_gate(30, (uint32_t)isr30, 0x08, 0x8E);
+  idt_set_gate(31, (uint32_t)isr31, 0x08, 0x8E);
 }
