2b621a774a5a2baa3038e9fd3ba48f710af25e90
Daniel Dybing <danieldy@uia.no>
2023-04-06 18:17:39 +0200

    Added gdt_set_entry function and comments

--- group_39/src/kernel/cpu/i386/gdt.cpp
+++ group_39/src/kernel/cpu/i386/gdt.cpp
@@ -8,4 +8,35 @@
 // Create an array of 3 GDT entries (NULL, Data and Text) 
 // as well as a GDT pointer on stack.
 gdt_entry gdt_entries[3];
-gdt_pointer gdt_ptr; 
+gdt_pointer gdt_ptr;
+
+
+
+
+
+gdt_entries[0] = {0,0,0,0,0,0}; // NULL descriptor
+gdt_entries[1] = {1,0,0xFFFFFFFF,0x9A, 0xCF}; 
+
+
+
+// gdt_set_entry is a function based on the lecturer' implmentation of 'gdt_set_gate'
+// This function takes the 
+gdt_set_entry (int32_t num, uint32_t base, uint32_t limit, uint8_t access, uint8_t granularity)
+{
+    // Set the base address fields
+    gdt_entries[num].base_low = (base & 0xFFFF);        // Set the lower 16 bits of the base address
+    gdt_entries[num].base_middle = (base >> 16) & 0xFF; // Set the middle 8 bits of the base address
+    gdt_entries[num].base_high = (base >> 24) & 0xFF;   // Set the upper 8 bits of the base address
+
+
+    // Set the limit field
+    gdt_entries[num].limit_low = (limit & 0xFFFF);
+
+    // Set the granularity fields
+    gdt_entries[num].granularity = (limit >> 16) & 0x0F;
+
+    gdt_entries[num].granularity |= granularity & 0xF0;
+
+    // Set the access field
+    gdt_entries[num].access = access;
+}
