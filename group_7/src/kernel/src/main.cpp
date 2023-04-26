568fe12fd216809fa1b874d629df8feb44748f1f
daniev <danievprogramming@gmail.com>
2023-04-26 08:14:11 +0200

    added file content from PerArne

--- group_7/src/kernel/src/main.cpp
+++ group_7/src/kernel/src/main.cpp
@@ -12,10 +12,71 @@
 extern "C"{
     // Calls the kernel_main assembly function.
     #include "system.h"
+    #include "memory.h"
 
     void kernel_main();
 }
 
+// Overload the new operator for single object allocation
+void* operator new(std::size_t size) {
+    return malloc(size);   // Call the C standard library function malloc() to allocate memory of the given size and return a pointer to it
+}
+
+// Overload the delete operator for single object deallocation
+void operator delete(void* ptr) noexcept {
+    free(ptr);             // Call the C standard library function free() to deallocate the memory pointed to by the given pointer
+}
+
+// Overload the new operator for array allocation
+void* operator new[](std::size_t size) {
+    return malloc(size);   // Call the C standard library function malloc() to allocate memory of the given size and return a pointer to it
+}
+
+// Overload the delete operator for array deallocation
+void operator delete[](void* ptr) noexcept {
+    free(ptr);             // Call the C standard library function free() to deallocate the memory pointed to by the given pointer
+}
+
+
+void demonstrate_page_fault(){
+
+    void (*callback)(registers_t&) = [](registers_t& regs)
+    {
+        // A page fault has occurred.
+        // The faulting address is stored in the CR2 register.
+        uint32_t faulting_address;
+        asm volatile("mov %%cr2, %0" : "=r" (faulting_address));
+
+        // The error code gives us details of what happened.
+        int present   = !(regs.err_code & 0x1); // Page not present
+        int rw = regs.err_code & 0x2;           // Write operation?
+        int us = regs.err_code & 0x4;           // Processor was in user-mode?
+        int reserved = regs.err_code & 0x8;     // Overwritten CPU-reserved bits of page entry?
+        int id = regs.err_code & 0x10;          // Caused by an instruction fetch?
+
+        // Output an error message.
+        printf("Page fault! ( ");
+        if (present) {printf("present ");}
+        if (rw) {printf("read-only ");}
+        if (us) {printf("user-mode ");}
+        if (reserved) {printf("reserved ");}
+        printf(") at 0x%x", faulting_address);
+        printf("\n");
+        panic("Page fault");
+    };
+
+    // Create interrupt handler for interrupt 14, which is page fault
+    register_interrupt_handler(14,[](registers_t* regs, void* context){
+        void (*callback)(registers_t*) = reinterpret_cast<void (*)(registers_t*)>(context);
+        callback(regs);
+
+        printf("Page Fault!\n");
+    }, (void*)callback);
+
+    uint32_t *ptr = (uint32_t*)0xA0000000;
+    uint32_t do_page_fault = *ptr;
+
+}
 
 /// @brief The kernel main function.
 void kernel_main()
