64e5024dd289b4b21e72a57a900b21dd5e6944ba
Daniel Dybing <danieldy@uia.no>
2023-04-30 12:35:37 +0200

    Implemented gdt.asm

--- group_39/src/kernel/cpu/i386/gdt.asm
+++ group_39/src/kernel/cpu/i386/gdt.asm
@@ -1,7 +1,7 @@
 ; Written by IKT218 Group 39 spring 2023
 
 ; This assembly code loads the GDT using the 'lgdt' x86 instruction. 
-; The following code is rewritten from both James Molloy OS development examples
+; The following code is written highly inspired by both James Molloy OS development examples
 ; and our lecturer's examples.
 
 
@@ -9,4 +9,16 @@
 [GLOBAL gdt_load] ;; This allows the function to be called from C code. 
 
 gdt_load: ;; This is the name of the function, which we can use to call the function from C code.
+    mov eax, [esp+4]
+    lgdt [eax]
 
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    jmp 0x08:.flush
+
+.flush:
+    ret
