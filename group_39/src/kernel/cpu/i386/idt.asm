0f43d503a231542ba2296e10ef3fec8b76fc4e69
Daniel Dybing <danieldy@uia.no>
2023-05-22 22:47:53 +0200

    Written the necessary code in idt.asm to load the IDT into memory

--- group_39/src/kernel/cpu/i386/idt.asm
+++ group_39/src/kernel/cpu/i386/idt.asm
@@ -4,7 +4,13 @@
 ; The following code is written highly inspired by both James Molloy OS development examples
 ; and our lecturer's examples.
 
+[GLOBAl idt_load] ; This allows us to call this subroutine from our C code. 
 
+; Defines the label of this subroutine. 
+idt_load:
+    mov eax, [esp+4] ; Move the parameter passed by the C code into eax register.
+    lidt [eax]       ; Load the IDT from the pointer stored in the eax register. 
+    ret              ; Return, so that control is passed back to the calling C code. 
 
 
 
