88dfb1a9cb8d0ecf8764abf8033e6967bbd8fda9
Daniel Dybing <danieldy@uia.no>
2023-05-22 22:20:59 +0200

    Added placeholder idt.asm file

---
+++ group_39/src/kernel/cpu/i386/idt.asm
@@ -0,0 +1,10 @@
+; Written by IKT218 Group 39 spring 2023
+
+; This assembly code loads the IDT using the 'lidt' x86 instruction. 
+; The following code is written highly inspired by both James Molloy OS development examples
+; and our lecturer's examples.
+
+
+
+
+
