bff232a29ec0c249ed149852f8d9a38e596136c5
Daniel Dybing <danieldy@uia.no>
2023-05-26 16:04:37 +0200

    Implemented memset function

---
+++ group_39/src/kernel/memory/memory.h
@@ -0,0 +1,18 @@
+// Written by group 39, IKT218 prosjekt, spring 2023
+
+// Implements memory functions such as memset.
+
+
+
+#ifndef UIA_MEMORY_H
+#define UIA_MEMORY_H
+
+void * memset(void * target, int value, unsigned int length);
+
+
+
+
+
+
+
+#endif
