b31ff12ba93ad626aa8d1aea6e785bf7e103287e
Daniel Dybing <daniel.dybing@me.com>
2023-05-26 11:34:39 +0200

    Removed typing error

--- group_39/src/kernel/cpu/i386/idt.cpp
+++ group_39/src/kernel/cpu/i386/idt.cpp
@@ -21,7 +21,7 @@ void idt_set_entry(uint8_t num, uint32_t base, uint16_t sel, uint8_t flags)
     idt_entries[num].base_high = (base >> 16) & 0xFFFF; // Set the upper 16 bits of the base address.
 
     idt_entries[num].selector = sel; // Set segment selector
-    idt_entries[num].reserved = 0; // Set the "alwaays0" field to 0. 
+    idt_entries[num].reserved = 0; // Set the "always0" field to 0. 
     idt_entries[num].flags   = flags | 0x60; // Set flags. 
 }
 
