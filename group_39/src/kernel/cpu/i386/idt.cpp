5c3ad2e9414c9283227529e52ba03e295a30b002
Daniel Dybing <danieldy@uia.no>
2023-05-28 11:34:58 +0200

    Added more comments to IDT.cpp

--- group_39/src/kernel/cpu/i386/idt.cpp
+++ group_39/src/kernel/cpu/i386/idt.cpp
@@ -1,9 +1,10 @@
 // Written by Group 39 IKT218 Project Spring 2023
 
 #include "idt.h"
+#include "../../memory/memory.h"
 
 // Number of IDT entries
-#define NUM_IDT_ENTRIES 256
+#define NUM_IDT_ENTRIES 3
 
 
 
@@ -22,15 +23,16 @@ void idt_set_entry(uint8_t num, uint32_t base, uint16_t sel, uint8_t flags)
 
     idt_entries[num].selector = sel; // Set segment selector
     idt_entries[num].reserved = 0; // Set the "always0" field to 0. 
-    idt_entries[num].flags   = flags | 0x60; // Set flags. 
+    idt_entries[num].flags    = flags | 0x60; // Set flags.
 }
 
-
+// This function sets up and loads the IDT. 
 void init_idt()
 {
     idt_pointer.base = (uint32_t)&idt_entries; // Set the base address of the IDT pointer.
     idt_pointer.limit = sizeof(idt_entry) * NUM_IDT_ENTRIES -1; // Set the limit of the IDT pointer. 
 
+    memset(&idt_entries, 0, sizeof(idt_entry)*NUM_IDT_ENTRIES); // Zero out all IDT entries, just in case they contain any garbage.
+    
 
-    //memset(&idt_entries, 0, sizeof(idt_entry)*NUM_IDT_ENTRIES);
 }
