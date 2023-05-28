f3ab8739a779be2e8d4ca024f64c5d3911a8a260
Ole <olejohanjo@uia.no>
2023-05-28 18:19:05 +0200

    cleanup and script update

--- group_1/src/kernel/src/main.cpp
+++ group_1/src/kernel/src/main.cpp
@@ -81,5 +81,7 @@ free(ptr); // Call the C standard library function free() to deallocate the memo
         printk("[%d]: Sleeping with interrupts (LOW CPU).\n", counter);
         sleep_interrupt(1000);
         printk("[%d]: Slept using interrupts.\n", counter++);
+
+
     }
 }
