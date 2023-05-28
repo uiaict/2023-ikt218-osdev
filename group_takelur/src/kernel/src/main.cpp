dc1ea36f11ed6b7722e35321260957dc9baaa927
Takelur <91278949+takelur@users.noreply.github.com>
2023-05-28 23:41:03 +0200

    Added printf

--- group_takelur/src/kernel/src/main.cpp
+++ group_takelur/src/kernel/src/main.cpp
@@ -4,6 +4,7 @@ extern "C" {
     #include "system.h"
     #include "monitor.h"
     #include "gdt.h"
+    #include "print.h"
     void kernel_main();
 }
 
@@ -12,23 +13,26 @@ void kernel_main() {
     init_gdt();
 
     clear_monitor();
-    monitor_write("Welcome to OS version 0.0.001!\n", 0, 15);
-    monitor_write("This should be on a new line\n\n", 0, 15);
-    monitor_write("Does tabs work? : 1\t2\t3\t4\t5\n", 0, 15);
-    monitor_write("Backspaces? : 1 \b2\b3 \b4\b5\n", 0, 15);
-    monitor_write("BG colors? : ", 0, 15);
-    monitor_write("RED ", 4, 15);
-    monitor_write("GREEN ", 2, 15);
-    monitor_write("BLUE \n", 1, 15);
-    monitor_write("FG colors? : ", 0, 15);
-    monitor_write("RED ", 0, 4);
-    monitor_write("GREEN ", 0, 2);
-    monitor_write("BLUE \n", 0, 1);
-    monitor_write("\nWill now test scrolling, should get line 1-25...\n", 0, 15);
+
+    printf("This should insert string: %s\n", "Hello World!");
+    printf("This should insert integer: %d\n", 123456789);
+    //monitor_write("Welcome to OS version 0.0.001!\n", 0, 15);
+    //monitor_write("This should be on a new line\n\n", 0, 15);
+    //monitor_write("Does tabs work? : 1\t2\t3\t4\t5\n", 0, 15);
+    //monitor_write("Backspaces? : 1 \b2\b3 \b4\b5\n", 0, 15);
+    //monitor_write("BG colors? : ", 0, 15);
+    //monitor_write("RED ", 4, 15);
+    //monitor_write("GREEN ", 2, 15);
+    //monitor_write("BLUE \n", 1, 15);
+    //monitor_write("FG colors? : ", 0, 15);
+    //monitor_write("RED ", 0, 4);
+    //monitor_write("GREEN ", 0, 2);
+    //monitor_write("BLUE \n", 0, 1);
+    //monitor_write("\nWill now test scrolling, should get line 1-25...\n", 0, 15);
     // Since no sleep function is implemented I will temporarily juse use a busy loop
-    for (int i = 0; i < 1000000000; i++) {
-        i = i + 1;
-        i = i - 1;
-    }
-    monitor_write("1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n21\n22\n23\n24\n25", 0, 15);
+    //for (int i = 0; i < 1000000000; i++) {
+    //    i = i + 1;
+    //    i = i - 1;
+    //}
+    //monitor_write("1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n21\n22\n23\n24\n25", 0, 15);
 }
--- group_takelur/src/stdlib/src/print.c
+++ group_takelur/src/stdlib/src/print.c
@@ -1,6 +1,7 @@
 #include <stdarg.h> // for va_list
 #include "monitor.h"
 
+
 // Reverses a string
 void reverse(char* str, int length)
 {
@@ -83,3 +84,46 @@ void print_char(char c) {
     monitor_put(c, 0, 15); // bg_color = 0 (black), fg_color = 15 (white)
 }
 
+void printf(char *str, ...)
+{
+    
+    va_list args;           // Declare list of arguments
+    va_start(args, str);    // Init list of arguments
+
+    // Go through the string, print as necessary
+    while (*str != '\0') {
+        if (*str == '%') { // If there is a format specifier
+            str++;
+            switch (*str) {
+                case 'd': { // Format for decimal integer
+                    int num = va_arg(args, int);
+                    print_int(num);
+                    break;
+                }
+                case 's': { // Format for string
+                    char *s = va_arg(args, char *);
+                    print_str(s);
+                    break;
+                }
+                case 'c': { // Format for character
+                    char c = va_arg(args, int); // Char is promoted to int when passed through the list
+                    print_char(c);
+                    break;
+                }
+                case '%': // Format for literal '%'
+                    print_char('%');
+                    break;
+                default:
+                    // Print both the '%' and the following character
+                    print_char('%');
+                    print_char(*str);
+                    break;
+            }
+        } else {
+            print_char(*str);
+        }
+        str++;
+    }
+
+    va_end(args);   // Clean up the list of arguments
+}
