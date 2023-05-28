bb404292366d53e14050038cd61db8e2a404376e
Ole <olejohanjo@uia.no>
2023-05-28 14:59:22 +0200

    added pit.h

--- group_1/src/stdlib/include/system.h
+++ group_1/src/stdlib/include/system.h
@@ -6,4 +6,3 @@ void print_char(char c, unsigned char color, unsigned int position);
 void itoa(int value, char* str, int base);
 void reverse(char* str, int length);
 void clear_screen();
-void write_string(unsigned char colour, const char *string);
