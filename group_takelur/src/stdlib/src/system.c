017b4066f1a3b47e15a08625fc3324a9ab723d44
Takelur <91278949+takelur@users.noreply.github.com>
2023-05-28 22:57:45 +0200

    Moved files

--- group_takelur/src/stdlib/src/system.c
+++ group_takelur/src/stdlib/src/system.c
@@ -1,1 +1,28 @@
 #include "system.h"
+
+// simplified printf version that supports %d, %s, %c and %%
+void printf(char *str, ...)
+{
+    char *arg = (char *) &str + sizeof(str);
+    while (*str != '\0') {
+        if (*str == '%') {
+            switch (*++str) {
+                case 'd':
+                    print_int(*((int *) arg++));
+                    break;
+                case 's':
+                    print_str(*((char **) arg++));
+                    break;
+                case 'c':
+                    print_char(*((char *) arg++));
+                    break;
+                case '%':
+                    print_char('%');
+                    break;
+            }
+        } else {
+            print_char(*str);
+        }
+        str++;
+    }
+}
