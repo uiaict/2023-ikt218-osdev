51b9cb10edb22e8202b394c378546fa50a63e463
sofievb <sofievb@uia.no>
2023-05-28 16:15:04 +0200

    h

--- group_tuva/src/kernel/include/common.h
+++ group_tuva/src/kernel/include/common.h
@@ -13,8 +13,19 @@ typedef          short s16int;
 typedef unsigned char  u8int;
 typedef          char  s8int;
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Declares the init_descriptor_tables function, which
+// initializes the GDT.
 void outb(u16int port, u8int value);
 u8int inb(u16int port);
 u16int inw(u16int port);
+// End of the extern C declaration
+#ifdef __cplusplus
+}
+#endif
+
 
 #endif // COMMON_H
