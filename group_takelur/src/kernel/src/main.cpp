0756ad3cae5bb3b1071d2c5266bf59edd6e68897
Takelur <91278949+takelur@users.noreply.github.com>
2023-05-29 22:58:31 +0200

    Added loop to print from keyboard buffer

--- group_takelur/src/kernel/src/main.cpp
+++ group_takelur/src/kernel/src/main.cpp
@@ -60,4 +60,11 @@ void kernel_main() {
     }
     monitor_write("1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n21\n22\n23\n24\n25", 0, 15);
     */
+
+   while(1)
+   {
+       char c = keyboard_read();
+         if (c != 0)
+              printf("%c", c);
+   };
 }
