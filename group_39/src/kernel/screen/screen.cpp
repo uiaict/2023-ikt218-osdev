de66ae8041949bbae01bd3c5070da6d83d4ebd20
Daniel Dybing <danieldy@uia.no>
2023-05-30 17:37:10 +0200

    Changed clearScreen function

--- group_39/src/kernel/screen/screen.cpp
+++ group_39/src/kernel/screen/screen.cpp
@@ -3,16 +3,20 @@
 
 
 #define VGA_ADDRESS 0xB8000 // The address of the VGA buffer.
-#define BUFSIZE 2200        // The buffer size of the VGA buffer.
+#define BUFSIZE 4000       // The buffer size of the VGA buffer (80x25 characters, 2 bytes per character)
 
 
 // This function clears the screen by writing blank spaces to the VGA memory.
 void clearScreen()
 {
+    // This defines a blank, black character to fill the screen with
+    uint16_t blankCharacter = 0x20; // Space
+    uint16_t blankColour = 0x0F ; // Black with white text
     uint8_t *vga = (uint8_t*) VGA_ADDRESS;
     for (int i = 0; i < BUFSIZE; i++)
     {
-        vga[i] = 0;
+        *vga++ = blankCharacter;
+        *vga++ = blankColour;
     }
 }
 
@@ -23,9 +27,11 @@ void printString(char * string)
     while(*string != 0)
     {
         *vgaBuffer++ = *string++; // Print a single character to the screen.
-        *vgaBuffer++ = 15; // Print text in colour 'WHITE'. 
+        *vgaBuffer++ = 15; // Print text in colour 'WHITE'.
     }
 }
+
+
 
 // This function writes a char to the VGA memory, so that it is displayed on the screen.
 void printChar(char ch)
