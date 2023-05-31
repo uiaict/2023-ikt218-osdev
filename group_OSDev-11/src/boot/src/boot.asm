cfda93e6b155a1e841e3f6fc8ca996cfb636830a
Ahmad Kais Sadat <ahmadks@uia.no>
2023-05-31 15:28:12 +0200

    updated

--- group_OSDev-11/src/boot/src/boot.asm
+++ group_OSDev-11/src/boot/src/boot.asm
@@ -1,65 +1,68 @@
 bits 32
 
 ; Configuration of Multiboot flags
-MBOOT_PAGE_ALIGN       equ 1<<0    ; Load kernel and modules on a page boundary
-MBOOT_MEM_INFO         equ 1<<1    ; Provide your kernel with memory info
+MBOOT_PAGE_ALIGN       equ 1<<0    ; Multiboot flag: Load kernel and modules on a page boundary
+MBOOT_MEM_INFO         equ 1<<1    ; Multiboot flag: Provide your kernel with memory info
 
 ; Definition of Multiboot header values
 MBOOT_AOUT_KLUDGE      equ 0x00010000;
 MBOOT_HEADER_MAGIC     equ 0x1BADB002 ; Multiboot Magic value
-MBOOT_HEADER_FLAGS     equ MBOOT_PAGE_ALIGN | MBOOT_MEM_INFO ;| MBOOT_VIDEO_MODE_BIT
-MBOOT_CHECKSUM         equ -(MBOOT_HEADER_MAGIC + MBOOT_HEADER_FLAGS)
+MBOOT_HEADER_FLAGS     equ MBOOT_PAGE_ALIGN | MBOOT_MEM_INFO ; Configure flags for Multiboot
+MBOOT_CHECKSUM         equ -(MBOOT_HEADER_MAGIC + MBOOT_HEADER_FLAGS) ; Checksum for Multiboot header
 
 ; Stack configuration. The size of our stack (16KB).
-KERNEL_STACK_SIZE equ 0x4000
+STACK_SIZE equ 0x4000
 
+; Multiboot section
 section .multiboot
 multiboot:
 align 4
-    dd  MBOOT_HEADER_MAGIC      ; GRUB will search for this value on each 4-byte boundary in your kernel file
-    dd  MBOOT_HEADER_FLAGS      ; How GRUB should load your file / settings
-    dd  MBOOT_CHECKSUM          ; To ensure that the above values are correct
+    dd  MBOOT_HEADER_MAGIC      ; Required for GRUB to recognize the file as Multiboot
+    dd  MBOOT_HEADER_FLAGS      ; Multiboot flags
+    dd  MBOOT_CHECKSUM          ; Ensure correct Multiboot header values
 
+		; Reserved fields
 		dd 0
 		dd 0
 		dd 0 
 		dd 0 
 		dd 0 
-    ; Graphic field
+
+    ; Graphics mode
     dd 0
     dd 640
     dd 480
     dd 32
 
+; Define the stack
 section .bss
 align 16
 stack_bottom:
-    resb KERNEL_STACK_SIZE ; 16 KiB
+    resb STACK_SIZE ; Reserve bytes for stack
 stack_top:
 
+; Entry point of our program
 section .text
 global _start:function (_start.end - _start)
 _start:
-	; extern init_multiboot
-	; push ebx ; multiboot_info struct
-	; push eax ; magic number
-	; call init_multiboot
-
-	
+	; Set the stack pointer
 	mov esp, stack_top
 
-	; initialize GDT
+	; Call the functions to initialize GDT and IDT
 	extern initialize_gdt
 	call initialize_gdt
-
-	; initialize IDT
 	extern initialize_idt
 	call initialize_idt
 
+	; Call the main function of the kernel
 	extern kernel_main
-	call kernel_main  ; call our kernel_main() function.
+	call kernel_main
+
+	; Disable interrupts
 	cli
     
-.hang:	hlt
+.hang:	
+	; Enter an infinite loop
+	hlt
 	jmp .hang
 .end:
