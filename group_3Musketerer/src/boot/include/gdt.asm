fbd1920abba12c3b609972f8ced218608a3b1802
Filip Guttormsen <filipg@uia.no>
2023-05-31 23:23:15 +0200

    temp: tried to fix asm file

--- group_3Musketerer/src/boot/include/gdt.asm
+++ group_3Musketerer/src/boot/include/gdt.asm
@@ -1,21 +1,16 @@
-;
-; Gdt.s -- contains global descriptor table and interrupt descriptor table
-;          setup code.
-;          Based on code from Bran's kernel development tutorials.
-;          Rewritten by JamesM's kernel development tutorials.
+section .text
+global _gdt_flush
+extern _gdt_ptr
 
-[GLOBAL gdt_flush]    ; Allows the C code to call gdt_flush().
-
-gdt_flush:
-    mov eax, [esp+4]  ; Get the pointer to the GDT, passed as a parameter.
-    lgdt [eax]        ; Load the new GDT pointer
-
-    mov ax, 0x10      ; 0x10 is the offset in the GDT to our data segment
-    mov ds, ax        ; Load all data segment selectors
+_gdt_flush:
+    lgdt [_gdt_ptr]
+    mov ax, 0x10
+    mov ds, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
     mov ss, ax
-    jmp 0x08:.flush   ; 0x08 is the offset to our code segment: Far jump!
-.flush:
+    jmp 0x08, flush
+
+flush:
     ret
