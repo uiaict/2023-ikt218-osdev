78be8dbc97d724978574daba19797ddf504e7dab
Ahmad Kais Sadat <ahmadks@uia.no>
2023-05-31 19:42:16 +0200

    commented idt.asm

--- group_OSDev-11/src/kernel/src/idt.asm
+++ group_OSDev-11/src/kernel/src/idt.asm
@@ -1,6 +1,6 @@
-[GLOBAL flush_idt]    ; Allows the C code to call flush_idt().
+[GLOBAL flush_idt]    ; Global label to allow the C code to call flush_idt().
 
 flush_idt:
     mov eax, [esp+4]  ; Get the pointer to the IDT, passed as a parameter.
     lidt [eax]        ; Load the IDT pointer.
-    ret
+    ret               ; Return from the function.
