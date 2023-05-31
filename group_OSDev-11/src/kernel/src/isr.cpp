84344a7c4989fdbfcb26073c6c1f6833d974292c
Ahmad Kais Sadat <ahmadks@uia.no>
2023-05-31 19:54:37 +0200

    commented isr.cpp

--- group_OSDev-11/src/kernel/src/isr.cpp
+++ group_OSDev-11/src/kernel/src/isr.cpp
@@ -3,21 +3,23 @@
 #include "terminal.h"
 #include <cstddef>
 
+// Forward declaration
 void isr_handler(registers regs) asm("isr_handler");
 
+// Assigns a handler function to a specific interrupt
 void assign_interrupt_handler(uint8_t n, isr_t handler, void* context)
 {
     int_handlers[n].handler = handler;
     int_handlers[n].data = context;
 }
 
-// Called from ASM interrupt handler stub
+// Handles ISR; is called from the assembly interrupt handler stub
 void isr_handler(registers regs)
 {
     uint8_t int_no = regs.int_no & 0xFF;
     int_handler intrpt = int_handlers[int_no];
 
-    if (intrpt.handler != 0) {
+    if (intrpt.handler) {
         intrpt.handler(&regs, intrpt.data);
     }
     else {
@@ -25,12 +27,13 @@ void isr_handler(registers regs)
     }
 }
 
+// Initializes the interrupt handlers
 void initialize_interrupt_handlers()
 {
     assign_interrupt_handler(3, [](registers* regs, void* data) {
         write_to_terminal("Interrupt 3 was triggered");
     }, NULL);
-    
+
     assign_interrupt_handler(4, [](registers* regs, void* data) {
         write_to_terminal("Interrupt 4 was triggered");
     }, NULL);
