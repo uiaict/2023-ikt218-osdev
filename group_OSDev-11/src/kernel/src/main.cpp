5c2ab234a1f0ec2c69b3c8b4ad0fb261270ef62c
Ahmad Kais Sadat <ahmadks@uia.no>
2023-05-31 17:51:50 +0200

    commit 7

--- group_OSDev-11/src/kernel/src/main.cpp
+++ group_OSDev-11/src/kernel/src/main.cpp
@@ -5,95 +5,91 @@
 #include "idt.h"
 #include "terminal.h"
 
-// Define VGA text mode buffer
-// Pointer to the memory-mapped VGA buffer that represents the screen
-volatile uint16_t* const VGA_BUFFER = (uint16_t*) 0xB8000; 
+// The VGA buffer's memory location, acting as the screen
+volatile uint16_t* const VGA_BUFFER = (uint16_t*) 0xB8000;
 
-// Constants to define the dimensions of the text module
+// Terminal dimensions in VGA text mode
 const size_t VGA_WIDTH = 80;
 const size_t VGA_HEIGHT = 25;
 
-// Initialize terminal
+// Keeping track of the current position within the terminal
+size_t terminal_row = 0;
+size_t terminal_col = 0;
+
+// Function to reset terminal, by filling it with spaces
 void initialize_terminal() {
-    
-	for (size_t i = 0; i < VGA_HEIGHT; i++) {
-        
-		for (size_t j = 0; j < VGA_WIDTH; j++) {
-			const size_t index = i * VGA_WIDTH + j; // Calculate index of current character
-            VGA_BUFFER[index] = ' ' | 0x0700; 		// Write a white character with black background and white foreground to the buffer
+    // Loop over all rows and columns, setting each position to a space character
+    for (size_t y = 0; y < VGA_HEIGHT; y++) {
+        for (size_t x = 0; x < VGA_WIDTH; x++) {
+            const size_t index = y * VGA_WIDTH + x;
+            VGA_BUFFER[index] = ' ' | 0x0700; // Write a space character with black background and white foreground
         }
     }
 }
 
-// Print string to terminal
+// Outputs a string to the terminal
 void write_to_terminal(const char* str) {
-    
-	size_t index = 0; // Index to keep track of current character
-
-    while (str[index]) { // Loop until null
-        
-		const size_t row = index / VGA_WIDTH; // Calculate row of current character
-        const size_t col = index % VGA_WIDTH; // Calculate column of current character
-        const size_t vga_index = row * VGA_WIDTH + col; // Calculate index in VGA buffer for current character
-        
-		VGA_BUFFER[vga_index] = ((uint16_t) str[index]) | 0x0700; // Write current character to the buffer
+    size_t index = 0;
+    // Iterate through each character of the string until null character
+    while (str[index]) {
+        const size_t row = index / VGA_WIDTH;
+        const size_t col = index % VGA_WIDTH;
+        const size_t vga_index = row * VGA_WIDTH + col;
+        // Write each character with black background and white foreground
+        VGA_BUFFER[vga_index] = ((uint16_t) str[index]) | 0x0700;
         index++;
     }
 }
 
-size_t terminal_row = 0;
-size_t terminal_col = 0;
-
+// Outputs a single character to the terminal, taking into account special characters
 void output_char_to_terminal(char c) {
-    
-    if (c == '\b') { // Backspace
+    if (c == '\b') { // Backspace character handling
+        // Move the cursor backwards, considering it might be at the beginning of a line
         if (terminal_col > 0) {
             terminal_col--;
-        } 
-        else if (terminal_row > 0) {
+        } else if (terminal_row > 0) {
             terminal_row--;
             terminal_col = VGA_WIDTH - 1;
         }
-        
         const size_t index = terminal_row * VGA_WIDTH + terminal_col;
-        VGA_BUFFER[index] = ((uint16_t) ' ') | 0x0700; // Overwrite with a space
-    } 
-    else if (c == '\n') { // Newline
+        VGA_BUFFER[index] = ((uint16_t) ' ') | 0x0700; // Replace the character at the cursor with a space
+    } else if (c == '\n') { // Newline character handling
         terminal_col = 0;
         terminal_row++;
-    } 
-    else { // Regular character
+    } else { // Other characters
         const size_t index = terminal_row * VGA_WIDTH + terminal_col;
-        VGA_BUFFER[index] = ((uint16_t) c) | 0x0700; // Write current character to the buffer
-
+        VGA_BUFFER[index] = ((uint16_t) c) | 0x0700;
         terminal_col++;
-
         if (terminal_col == VGA_WIDTH) {
             terminal_col = 0;
             terminal_row++;
         }
     }
+    // If the cursor has reached the end of the terminal, clear the terminal and reset the cursor
     if (terminal_row == VGA_HEIGHT) {
-        initialize_terminal(); 
+        initialize_terminal();
         terminal_row = 0;
         terminal_col = 0;
     }
 }
 
-// Define entry point in asm to prevent C++ mangling
+// Main function declaration, using extern to prevent C++ name mangling
 extern "C"{
     void kernel_main();
 }
 
+// Main function definition
 void kernel_main()
 {
+    // Initialize terminal and display a string
     initialize_terminal();
-    write_to_terminal("Hello World");
-    
+    write_to_terminal("Hello, World!");
+
+    // Trigger software interrupts
     asm volatile("int $0x3");
     asm volatile("int $0x4");
-    //asm volatile("int $0x5");
 
-   initialize_terminal();
-   while(true){};
+    // Clear terminal and enter an infinite loop
+    initialize_terminal();
+    while(true){};
 }
