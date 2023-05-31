2afb6cc5444bfab212d0e67a9a849471034dbec4
Ahmad Kais Sadat <ahmadks@uia.no>
2023-05-31 19:40:31 +0200

    commented idt.cpp

--- group_OSDev-11/src/kernel/src/idt.cpp
+++ group_OSDev-11/src/kernel/src/idt.cpp
@@ -49,6 +49,7 @@ void configure_interrupt_gate(uint8_t num, uint32_t base, uint16_t sel, uint8_t 
 }
 
 
+// Array of function pointers to the interrupt service routines (ISRs).
 void (*isr_functions[32])(void) = {
     isr0, isr1, isr2, isr3, isr4, isr5, isr6, isr7, 
     isr8, isr9, isr10, isr11, isr12, isr13, isr14, isr15, 
@@ -56,6 +57,7 @@ void (*isr_functions[32])(void) = {
     isr24, isr25, isr26, isr27, isr28, isr29, isr30, isr31
 };
 
+// Array of function pointers to the interrupt request (IRQ) handlers.
 void (*irq_functions[16])(void) = {
     irq0, irq1, irq2, irq3, irq4, irq5, irq6, irq7, 
     irq8, irq9, irq10, irq11, irq12, irq13, irq14, irq15
@@ -63,22 +65,26 @@ void (*irq_functions[16])(void) = {
 
 void initialize_interrupts()
 {
-    write_to_port(0x20, 0x11);
-    write_to_port(0xA0, 0x11);
-    write_to_port(0x21, 0x20);
-    write_to_port(0xA1, 0x28);
-    write_to_port(0x21, 0x04);
-    write_to_port(0xA1, 0x02);
-    write_to_port(0x21, 0x01);
-    write_to_port(0xA1, 0x01);
-    write_to_port(0x21, 0x0);
-    write_to_port(0xA1, 0x0);
+    // Initiate the Programmable Interrupt Controllers (PICs) with a 4-step initialization sequence.
+    write_to_port(0x20, 0x11); // Initialize the master PIC.
+    write_to_port(0xA0, 0x11); // Initialize the slave PIC.
+    write_to_port(0x21, 0x20); // Set the offset of the master PIC to 0x20.
+    write_to_port(0xA1, 0x28); // Set the offset of the slave PIC to 0x28.
+    write_to_port(0x21, 0x04); // Tell the master PIC there is a slave PIC at IRQ2.
+    write_to_port(0xA1, 0x02); // Tell the slave PIC its cascade identity (IRQ2).
+    write_to_port(0x21, 0x01); // Set mode of the master PIC.
+    write_to_port(0xA1, 0x01); // Set mode of the slave PIC.
+    write_to_port(0x21, 0x0);  // Unmask interrupts on the master PIC.
+    write_to_port(0xA1, 0x0);  // Unmask interrupts on the slave PIC.
 
+    // Configure the interrupt gates for the ISRs.
     for(int i = 0; i < 32; i++) {
         configure_interrupt_gate(i, (uint32_t)isr_functions[i], 0x08, 0x8E);
     }
 
+    // Configure the interrupt gates for the IRQ handlers.
     for(int i = 0; i < 16; i++) {
         configure_interrupt_gate(i + 32, (uint32_t)irq_functions[i], 0x08, 0x8E);
     }
 }
+
