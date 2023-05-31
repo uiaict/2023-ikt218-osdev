8e0ab386b448fa81652f2399626ab29d6302ff05
thomasbr <thomasbr@uia.no>
2023-06-01 01:01:01 +0200

    context

--- group_3Musketerer/src/kernel/src/main.cpp
+++ group_3Musketerer/src/kernel/src/main.cpp
@@ -13,7 +13,12 @@ extern "C"{
 
 void kernel_main()
 {
-    init_gdt();
+    /*Vi trenger ikke ha init_gdt i main siden vi kaller den i boot.asm, er ingen synnlig forskjell
+    Tror eneste feilen pt er at det er undefined reference på den, vet ikke helt hvorfor for å være helt ørlig
+    den burde ligge i system.h og ikke at vi inkluderer gdt.h her, tror det er samme problem som jeg 
+    hadde med write_terminal
+    */ 
+    //init_gdt();
     print_clear();
     print_str("Hello World");
     print_str("\n");
