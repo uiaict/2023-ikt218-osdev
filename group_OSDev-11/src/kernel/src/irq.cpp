cea9184ca9abbcb6c3f57338237b3fadd4ed2042
Ahmad Kais Sadat <ahmadks@uia.no>
2023-05-31 21:40:07 +0200

    update

--- group_OSDev-11/src/kernel/src/irq.cpp
+++ group_OSDev-11/src/kernel/src/irq.cpp
@@ -1,15 +1,38 @@
+#include <cstddef>
+#include <stdint.h>
 #include "idt.h"
 #include "terminal.h"
 #include "common.h"
-#include <cstddef>
-#include <stdint.h>
 
 extern "C" {
     #include "terminal.h"
-    void irq_handler(registers regs) asm("irq_handler");  // External C function declaration
+    void irq_handler(registers regs) asm("irq_handler"); // External C function
 }
 
-// Initialize IRQ handlers to default values
+// Function to assign a specific handler to an IRQ
+void assign_irq_handler(int irq, isr_t handler, void *ctx) 
+{
+    irq_handlers[irq].handler = handler;
+    irq_handlers[irq].data = ctx;
+}
+
+// IRQ handler function
+void irq_handler(registers regs)
+{
+    // If int_no >=40, send reset signal to slave
+    if (regs.int_no >= 40) write_to_port(0xA0, 0x20);
+
+    // EOI signal to master PIC
+    write_to_port(0x20, 0x20);
+
+    // Retrieve interrupt handler
+    int_handler intrpt = irq_handlers[regs.int_no];
+
+    // If handler is assigned, execute it
+    if (intrpt.handler != 0) intrpt.handler(&regs, intrpt.data);
+}
+
+// Initialize all IRQ handlers
 void initialize_irq()
 {
     for (int i = 0; i < IRQ_COUNT; i++) {
@@ -19,48 +42,15 @@ void initialize_irq()
     }
 }
 
-// Assign a handler to a specific IRQ
-void assign_irq_handler(int irq, isr_t handler, void *ctx) 
-{
-    irq_handlers[irq].handler = handler;
-    irq_handlers[irq].data = ctx;
-}
-
-// Define the IRQ handler function
-void irq_handler(registers regs)
-{
-    // If interrupt number is >=40, reset signal to slave
-    if (regs.int_no >= 40) {
-        write_to_port(0xA0, 0x20); 
-    }
-
-    // Send End Of Interrupt to master
-    write_to_port(0x20, 0x20); 
-
-    // Fetch the interrupt handler
-    int_handler intrpt = irq_handlers[regs.int_no];
-
-    // If there is a handler, execute it
-    if (intrpt.handler != 0) {
-        intrpt.handler(&regs, intrpt.data);
-    }
-}
-
-// Define a function to initialize IRQ handlers
+// Initialize IRQ handlers and enable interrupts
 void initialize_irq_handlers()
 {
-    // Turn on interrupts
-    asm volatile("sti");
+    asm volatile("sti"); // Allow interrupts
 
-    // Register IRQ handler for IRQ1
+    // Set up IRQ1 handler
     assign_irq_handler(IRQ1, [](registers*, void*) 
     {
-        // Fetch scancode
         uint8_t scancode = read_byte_from_port(0x60);
-
-        // If scancode is within key_map size, output character to terminal
-        if (scancode < sizeof(key_map)) {
-            output_char_to_terminal(key_map[scancode]);
-        }
+        if (scancode < sizeof(key_map)) output_char_to_terminal(key_map[scancode]);
     }, NULL);
 }
--- group_OSDev-11/src/kernel/src/isr.cpp
+++ group_OSDev-11/src/kernel/src/isr.cpp
@@ -6,22 +6,6 @@
 // Forward declaration
 void isr_handler(registers regs) asm("isr_handler");
 
-// Initializes the interrupt handlers
-void initialize_interrupt_handlers()
-{
-    assign_interrupt_handler(3, [](registers* regs, void* data) {
-        write_to_terminal("Interrupt 3 has been triggered!");
-    }, nullptr);
-
-    assign_interrupt_handler(4, [](registers* regs, void* data) {
-        write_to_terminal("Interrupt 4 has been triggered!");
-    }, nullptr);
-
-    assign_interrupt_handler(5, [](registers* regs, void* data) {
-        write_to_terminal("Interrupt 5 has been triggered!");
-    }, nullptr);
-}
-
 // Assigns a handler function to a specific interrupt
 void assign_interrupt_handler(uint8_t n, isr_t handler, void* context)
 {
@@ -35,9 +19,25 @@ void isr_handler(registers regs)
     uint8_t int_no = regs.int_no & 0xFF;
     int_handler intrpt = int_handlers[int_no];
 
-    if (intrpt.handler) {
+    if (intrpt.handler != 0) {
         intrpt.handler(&regs, intrpt.data);
     } else {
-        write_to_terminal("Interrupt not handled.");
+        write_to_terminal("Unhandled interrupt");
     }
+}
+
+// Initializes the interrupt handlers
+void initialize_interrupt_handlers()
+{
+    assign_interrupt_handler(3, [](registers* regs, void* data) {
+        write_to_terminal("Interrupt 3 has been triggered!");
+    }, NULL);
+
+    assign_interrupt_handler(4, [](registers* regs, void* data) {
+        write_to_terminal("Interrupt 4 has been triggered!");
+    }, NULL);
+
+    assign_interrupt_handler(5, [](registers* regs, void* data) {
+        write_to_terminal("Interrupt 5 has been triggered!");
+    }, NULL);
 }
