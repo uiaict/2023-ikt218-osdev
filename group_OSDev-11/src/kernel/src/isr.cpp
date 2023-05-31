72bd6d0728f51a3162dd35734ecf17a9e1b9ed56
Ahmad Kais Sadat <ahmadks@uia.no>
2023-05-31 22:08:56 +0200

    commit 69

--- group_OSDev-11/src/kernel/src/isr.cpp
+++ group_OSDev-11/src/kernel/src/isr.cpp
@@ -3,6 +3,7 @@
 #include "terminal.h"
 #include <cstddef>
 
+// Forward declaration
 void isr_handler(registers regs) asm("isr_handler");
 
 // Assigns a handler function to a specific interrupt
@@ -10,6 +11,22 @@ void assign_interrupt_handler(uint8_t n, isr_t handler, void* context)
 {
     int_handlers[n].handler = handler;
     int_handlers[n].data = context;
+}
+
+// Initializes the interrupt handlers
+void initialize_interrupt_handlers()
+{
+    assign_interrupt_handler(3, [](registers* regs, void* data) {
+        write_to_terminal("Interrupt 3 has been triggered!");
+    }, NULL);
+
+    assign_interrupt_handler(4, [](registers* regs, void* data) {
+        write_to_terminal("Interrupt 4 has been triggered!");
+    }, NULL);
+
+    assign_interrupt_handler(5, [](registers* regs, void* data) {
+        write_to_terminal("Interrupt 5 has been triggered!");
+    }, NULL);
 }
 
 // Handles ISR; called from the assembly interrupt handler stub
@@ -20,24 +37,8 @@ void isr_handler(registers regs)
 
     if (intrpt.handler != 0) {
         intrpt.handler(&regs, intrpt.data);
-    }
-    else {
-        write_to_terminal("Unhandled interrupt");
+    } else {
+        write_to_terminal("Interrupt not handled.");
     }
 }
 
-// Initializes the interrupt handlers
-void initialize_interrupt_handlers()
-{
-    assign_interrupt_handler(3, [](registers* regs, void* data) {
-        write_to_terminal("Interrupt 3 was triggered");
-    }, NULL);
-    
-    assign_interrupt_handler(4, [](registers* regs, void* data) {
-        write_to_terminal("Interrupt 4 was triggered");
-    }, NULL);
-
-    assign_interrupt_handler(5, [](registers* regs, void* data) {
-        write_to_terminal("Interrupt 5 was triggered");
-    }, NULL);
-}
