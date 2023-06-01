e1ca28f6ccb273c0a3e0748a1c7c2f1ef0330e51
Jon Larssen <jona.larss@gmail.com>
2023-06-01 11:52:01 +0200

    prepared merge

---
+++ group_name/src/kernel/cpu/gdt.h
@@ -0,0 +1,26 @@
+#ifndef UIAOS_GDT_H
+#define UIAOS_GDT_H
+
+#include <stdint.h>
+#include "screen.h"
+
+namespace UIAOS::CPU::GDT {
+  struct gdt_entry {
+    uint16_t limit_low;
+    uint16_t base_low;
+    uint8_t base_middle;
+    uint8_t access;
+    uint8_t granularity;
+    uint8_t base_high;
+  } __attribute__((packed));
+
+  struct gdt_ptr {
+    uint16_t limit;
+    uint32_t base;
+  } __attribute__((packed));
+
+  void gdt_init();
+  void gdt_setGate(int32_t num, uint32_t base, uint32_t limit, uint8_t access, uint8_t gran);
+} 
+
+#endif //UIAOS_GDT_H
