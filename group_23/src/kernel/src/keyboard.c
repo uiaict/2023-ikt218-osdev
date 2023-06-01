1fbe07055210e6ff43b910f4b288b01a0e5ca0af
eid96 <101329232+eid96@users.noreply.github.com>
2023-06-01 11:59:43 +0200

    Update keyboard.c
    Clean up in keyboard

--- group_23/src/kernel/src/keyboard.c
+++ group_23/src/kernel/src/keyboard.c
@@ -10,18 +10,7 @@
 #define ENTER 0x1C
 static char key_buffer[256];
 
-/*const char *sc_name[] = { "ERROR", "Esc", "1", "2", "3", "4", "5", "6", 
-    "7", "8", "9", "0", "-", "=", "Backspace", "Tab", "Q", "W", "E", 
-        "R", "T", "Y", "U", "I", "O", "P", "[", "]", "Enter", "Lctrl", 
-        "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "`", 
-        "LShift", "\\", "Z", "X", "C", "V", "B", "N", "M", ",", ".", 
-        "/", "RShift", "Keypad *", "LAlt", "Spacebar"};
 
-const char sc_ascii[] = { '?', '?', '1', '2', '3', '4', '5', '6',     
-    '7', '8', '9', '0', '-', '=', '?', '?', 'Q', 'W', 'E', 'R', 'T', 'Y', 
-        'U', 'I', 'O', 'P', '[', ']', '?', '?', 'A', 'S', 'D', 'F', 'G', 
-        'H', 'J', 'K', 'L', ';', '\'', '`', '?', '\\', 'Z', 'X', 'C', 'V', 
-        'B', 'N', 'M', ',', '.', '/', '?', '?', '?', ' '};*/
 
 static void keyboard_callback(registers_t *regs){
     uint8_t scancode= inb(0x60);
@@ -32,11 +21,10 @@ static void keyboard_callback(registers_t *regs){
     }
     
     else{
-        //char *sc_ascii;
-        //int_to_ascii(scancode, sc_ascii);
+       
         monitor_put('\n');
         monitor_write("Keyboard scancode: ");
-        //monitor_write(sc_ascii);
+        
         print_letter(scancode);
         
     }
