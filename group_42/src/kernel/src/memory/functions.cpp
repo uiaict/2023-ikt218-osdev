6fab3a74c5687039036383c7ef99e3776c7d9e50
Jon Ingvar <joningvarjs@gmail.com>
2023-06-01 20:57:19 +0200

    more comments

--- group_42/src/kernel/src/memory/functions.cpp
+++ group_42/src/kernel/src/memory/functions.cpp
@@ -8,80 +8,74 @@ extern "C"{
 #include "paging.h"
 #include "memory.h"
 #include"../PIT/pit.h"
-static uint32_t* page_directory = 0;   // Define a pointer to the page directory and initialize it to zero
-static uint32_t page_dir_loc = 0;      // Define the location of the page directory and initialize it to zero
-static uint32_t* last_page = 0;        // Define a pointer to the last page and initialize it to zero
+// start by setting global values who that is not sett to a value at this time
+static uint32_t* page_directory = 0;   // pointer to not sett page directory
+static uint32_t page_dir_loc = 0;     // location of page directory not sett
+static uint32_t* last_page = 0;        // pointer to the last page not sett 
 
-/* Paging now will be really simple
- * we reserve 0-8MB for kernel stuff
- * heap will be from approx 1mb to 4mb
- * and paging stuff will be from 4mb
- */
 
-// Function to map virtual addresses to physical addresses
-void paging_map_virtual_to_phys(uint32_t virt, uint32_t phys)
+void paging_map_virtual_to_phys(uint32_t virt, uint32_t phys)  // function that maps virtual adresses to physical adresses
 {
-    uint16_t id = virt >> 22;        // Get the upper 10 bits of the virtual address to use as index in the page directory
-    for(int i = 0; i < 1024; i++)   // Loop through all 1024 page table entries
+    uint16_t id = virt >> 22;        // get the page id from the virtual adress
+    for(int i = 0; i < 1024; i++)  // look att all pages
     {
-        last_page[i] = phys | 3;    // Set the page table entry to the physical address with the present and write permissions set
-        phys += 4096;               // Increment the physical address by the page size of 4 KB
-    }
-    page_directory[id] = ((uint32_t)last_page) | 3;  // Set the page directory entry for the virtual address to the physical address of the page table with present and write permissions set
-    last_page = (uint32_t *)(((uint32_t)last_page) + 4096); // Move to the next page in memory
+        last_page[i] = phys | 3;    // sets page entry to to physical adress 
+        phys += 4096;              // increment physical adress by page size
+    } 
+    page_directory[id] = ((uint32_t)last_page) | 3;  
+    last_page = (uint32_t *)(((uint32_t)last_page) + 4096); // move to next page
 }
 
-// Function to enable paging
-void paging_enable()
+void paging_enable() // function to enable paging
 {
-    asm volatile("mov %%eax, %%cr3": :"a"(page_dir_loc)); // Load the physical address of the page directory into the CR3 register
-    asm volatile("mov %cr0, %eax");         // Load the CR0 register into the EAX register
-    asm volatile("orl $0x80000000, %eax");  // Set the paging enable bit in the EAX register
-    asm volatile("mov %eax, %cr0");         // Load the EAX register into the CR0 register to enable paging
+    asm volatile("mov %%eax, %%cr3": :"a"(page_dir_loc)); 
+    asm volatile("mov %cr0, %eax");         
+    asm volatile("orl $0x80000000, %eax"); 
+    asm volatile("mov %eax, %cr0");         
 }
 
-// Function to initialize paging
-void init_paging()
+
+void init_paging() // function that inizelize paging
 {
-    //<<printf("Setting up paging\n");
-    page_directory = (uint32_t*)0x400000;      // Set the page directory to start at 4 MB
-    page_dir_loc = (uint32_t)page_directory;  // Set the physical address of the page directory
-    last_page = (uint32_t *)0x404000;         // Set the last page to start at 4 MB + 4 KB
-    for(int i = 0; i < 1024; i++)             // Loop through all 1024 page directory entries
+
+    page_directory = (uint32_t*)0x400000;    // page directory starts at 4 MB  
+    page_dir_loc = (uint32_t)page_directory;  // sets the physical adress of page directory
+    last_page = (uint32_t *)0x404000;        // last page starts at 4 MB + 4MB
+    for(int i = 0; i < 1024; i++)            // loop through all pages
     {
-        page_directory[i] = 0 | 2;            // Set the page directory entry to not present with supervisor level read/write permissions
+        page_directory[i] = 0 | 2;           // set the page directory entry to no page
     }
-    paging_map_virtual_to_phys(0, 0);         // Map the first 4 MB of virtual memory to the first 4 MB of physical memory
-    paging_map_virtual_to_phys(0x400000, 0x400000); // Map the next 4 MB of virtual memory to the next 4 MB of physical memory
-    paging_enable();                          // Enable paging
-    //printf("Paging was successfully enabled!\n");
+    paging_map_virtual_to_phys(0, 0);       // map first 4 MB of virutal memory to first 4MB of physical
+    paging_map_virtual_to_phys(0x400000, 0x400000); // same for the next 4 MB
+    paging_enable();                         // asambly fuction in cpp to enable paging
+  
 }
 
  extern uint32_t end;
- void init_kernel(){
+ void init_kernel(){// help function since we for some reason are unable to call the function direcly
    
     init_kernel_memory(&end);
  }
 
- 
-void* operator new(std::size_t size){
+// functions from task 
+void* operator new(std::size_t size){ // this function allows creating a new object
     return malloc(size);
 }
-void* operator new[](std::size_t size){
+void* operator new[](std::size_t size){// this function allows creating a new array
     return malloc(size);
 } 
-void operator delete(void* ptr) noexcept{
+void operator delete(void* ptr) noexcept{// this function removing creating a  object
     free(ptr);
 }
 
-void operator delete[](void* ptr) noexcept{
+void operator delete[](void* ptr) noexcept{// this function removing creating a  array
     free(ptr);
 }
-void init_pag(){
+void init_pag(){// help function since we for some reason are unable to call the function direcly
     init_paging();
 }
 
 
-void start_sleep_busy(uint32_t milliseconds){
+void start_sleep_busy(uint32_t milliseconds){// help function since we for some reason are unable to call the function direcly
     sleep_busy(milliseconds);
 }
