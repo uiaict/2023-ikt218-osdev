d24d52de2f6cb6def166aa6681fdafc352491926
Mathias René Juelsen-Jacobsen <mathiasrj@uia.no>
2023-06-02 00:34:10 +0200

    Fixed typo in GDT

--- group_32/src/kernel/src/gdt/gdt.h
+++ group_32/src/kernel/src/gdt/gdt.h
@@ -7,7 +7,6 @@
 
 #define GDT_ENTRIES 5
 
-
 // Define the GDT entry structure
 struct gdt_entry_t {
   uint16_t limit_low;
@@ -18,37 +17,19 @@ struct gdt_entry_t {
   uint8_t base_high;
 } __attribute__((packed));
 
-// Define the IDT entry structure
-struct idt_entry_t {
-  uint16_t base_low;
-  uint16_t selector;
-  uint8_t zero;
-  uint8_t flags;
-  uint16_t base_high;
-} __attribute__((packed));
-
-// Define the GDT and IDT pointers
+// Define the GDT pointers
 struct gdt_ptr_t {
   uint16_t limit;
   uint32_t base;
 } __attribute__((packed));
 
-
-
-
-// Initialize the GDT and IDT
+// Initialize the GDT
 void init_gdt();
 
-
-// Load the GDT and IDT
+// Load the GDT
 void gdt_load();
 
-
 void gdt_set_gate(int32_t num, uint32_t base, uint32_t limit, uint8_t access, uint8_t gran);
-
-
-
-
 
 static gdt_entry_t gdt[GDT_ENTRIES];
 static gdt_ptr_t gdt_ptr;
