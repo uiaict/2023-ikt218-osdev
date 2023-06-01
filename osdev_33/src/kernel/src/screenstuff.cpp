74d7b94097c09dafc12c079b3439b15bb19f9e2f
Håvard Willoch Bergsvik <haavardwb@uia.no>
2023-06-01 15:15:27 +0200

    finito assignment 2

--- osdev_33/src/kernel/src/screenstuff.cpp
+++ osdev_33/src/kernel/src/screenstuff.cpp
@@ -11,12 +11,25 @@ void clear_screen()
 }
 
 // Define write_string function
-void write_string( int colour, const char *string )
+void write_string( int line, int column, int colour, const char *string )
 {
-    volatile char *video = (volatile char*)0xB84AA;
+    volatile char *video = (volatile char*)(0xB8000 + line * 160 + column * 2);
     while( *string != 0 )
     {
         *video++ = *string++;
         *video++ = colour;
+    }
+}
+
+
+
+void print_pattern(const char* pattern[], int num_lines, int start_pos) {
+    for (int i = 0; i < num_lines; i++) {
+        const char* line = pattern[i];
+        volatile char *video = (volatile char*)(0xB8000 + (start_pos + i) * 160);
+        while (*line != 0) {
+            *video++ = *line++;
+            *video++ = 10; 
+        }
     }
 }
--- osdev_33/src/kernel/src/screenstuff.h
+++ osdev_33/src/kernel/src/screenstuff.h
@@ -1,4 +1,5 @@
 #include <stdint.h>
 
 void clear_screen();
-void write_string( int colour, const char *string );
+void write_string( int line, int column, int colour, const char *string );
+void print_pattern(const char* pattern[], int num_lines, int start_pos);
