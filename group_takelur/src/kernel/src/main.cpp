71d8a5ac7dbcebfd008f6088e34a1d63d797c992
Takelur <91278949+takelur@users.noreply.github.com>
2023-06-01 17:19:34 +0200

    Polished visually

--- group_takelur/src/kernel/src/main.cpp
+++ group_takelur/src/kernel/src/main.cpp
@@ -91,32 +91,40 @@ void kernel_main()
 
 
     /* PRINT TESTS */
-    //printf("This should insert string: %s\n", "Hello World!");
-    //printf("This should insert integer: %d\n", 12345);
-    //printf("This should insert decimal %d as hex: %x\n", 1452, 1452);
-    //printf("This should insert percentage char: %%\n");
-    
+    /*
+    clear_monitor();
+    printf("This should insert string: %s\n", "Hello World!");
+    printf("This should insert integer: %d\n", 12345);
+    printf("This should insert decimal %d as hex: %x\n", 1452, 1452);
+    printf("This should insert percentage char: %%\n");
+    sleep_interrupt(6000);
+    */
+
 
 
     /* INTERRUPT TESTS
     I have not implementet any actual exception handling. My service routines only print information about which interrupt
     was triggered, and do not handle the fault. These tests therefore only triggers the ISR, and do not create an actual exception.
     */
-    //printf("Triggering divide by zero exception...\n");
-    //test_divide_by_zero();
-    //printf("Triggering debug exception...\n"); 
-    //trigger_debug_exception();
-    //printf("Triggering breakpoint exception...\n");
-    //trigger_breakpoint_exception();
-    //printf("Triggering invalid opcode exception...\n");
-    //trigger_invalid_opcode_exception();
-    //printf("Triggering unhandled interrupt exception...\n");
-    //trigger_unhandled_interrupt(); // Trigger interrupt 27, which only has the default handler I created and is not set explicitly
+    /*
+    printf("Triggering divide by zero exception...\n");
+    test_divide_by_zero();
+    printf("Triggering debug exception...\n"); 
+    trigger_debug_exception();
+    printf("Triggering breakpoint exception...\n");
+    trigger_breakpoint_exception();
+    printf("Triggering invalid opcode exception...\n");
+    trigger_invalid_opcode_exception();
+    printf("Triggering unhandled interrupt exception...\n");
+    trigger_unhandled_interrupt(); // Trigger interrupt 27, which only has the default handler I created and is not set explicitly
+    sleep_interrupt(6000);
+    */
 
 
-
-    /* MONITOR WRITING TESTS (assignment 2)
-    monitor_write("Welcome to Tåkelur OS version 0.1!\n", 0, 15);
+    /* MONITOR WRITING TESTS (assignment 2) */
+    /*
+    clear_monitor();
+    monitor_write("Welcome to Takelur OS version 0.1!\n", 0, 15);
     monitor_write("This should be on a new line\n\n", 0, 15);
     monitor_write("Does tabs work? : 1\t2\t3\t4\t5\n", 0, 15);
     monitor_write("Backspaces? : 1 \b2\b3 \b4\b5\n", 0, 15);
@@ -128,12 +136,10 @@ void kernel_main()
     monitor_write("RED ", 0, 4);
     monitor_write("GREEN ", 0, 2);
     monitor_write("BLUE \n", 0, 1);
-    monitor_write("\nWill now test scrolling, should get line 1-25...\n", 0, 15);
-     Since no sleep function is implemented I will temporarily juse use a busy loop
-    for (int i = 0; i < 1000000000; i++) {
-        i = i + 1;
-    }
+    monitor_write("\nWill now test scrolling, should get line 1-25 in 10 seconds...\n", 0, 15);
+    sleep_interrupt(10000);
     monitor_write("1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n21\n22\n23\n24\n25", 0, 15);
+    sleep_interrupt(6000);
     */
 
 
@@ -151,24 +157,6 @@ void kernel_main()
         sleep_interrupt(1000);
         printf("[%d]: Slept using interrupts.\n", counter++);
     }
-    */
-
-
-
-    /* MONITOR WRITING TESTS (assignment 2) */
-    /*
-    monitor_write("Welcome to Tåkelur OS version 0.1!\n", 0, 15);
-    monitor_write("This should be on a new line\n\n", 0, 15);
-    monitor_write("Does tabs work? : 1\t2\t3\t4\t5\n", 0, 15);
-    monitor_write("Backspaces? : 1 \b2\b3 \b4\b5\n", 0, 15);
-    monitor_write("BG colors? : ", 0, 15);
-    monitor_write("RED ", 4, 15);
-    monitor_write("GREEN ", 2, 15);
-    monitor_write("BLUE \n", 1, 15);
-    monitor_write("FG colors? : ", 0, 15);
-    monitor_write("RED ", 0, 4);
-    monitor_write("GREEN ", 0, 2);
-    monitor_write("BLUE \n", 0, 1);
     */
 
 
@@ -208,10 +196,10 @@ void kernel_main()
         printf("Finished playing the song.\n");
     }
     */
+    
 
 
-
-   // While loop to read from keyboard and print to monitor
+    // While loop to read from keyboard and print to monitor
     while(1)
     {
          char c = keyboard_read();
