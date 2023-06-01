516a6e5139acb885cb7e094d366e9997fb266360
Gustav Halvorsen <gustavh@student.uia.no>
2023-06-01 10:51:10 +0200

    Changed terminal color back to black and white

--- group_voliboar/src/kernel/src/screen.cpp
+++ group_voliboar/src/kernel/src/screen.cpp
@@ -50,7 +50,7 @@ void terminal_initialize(void)
 {
 	terminal_row = 0;
 	terminal_column = 0;
-	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_RED, VGA_COLOR_GREEN);
+	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
 	terminal_buffer = (uint16_t*) 0xB8000;
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
