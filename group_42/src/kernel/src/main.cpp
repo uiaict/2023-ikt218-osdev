bf8d70672d07e1f00cab658c4faba2b2fed9cd5e
Isak Killingrød <isakk@uia.no>
2023-06-02 18:56:01 +0200

    Spellchecked main

--- group_42/src/kernel/src/main.cpp
+++ group_42/src/kernel/src/main.cpp
@@ -14,13 +14,13 @@ extern "C"{
 void kernel_main()
 {   
     
-   init_pag(); // inzisilize paging for memory management
+   init_pag(); // Initialize paging for memory management
    auto os = IJI_OS(); 
-   os.init(); // clear the screen and write hello
-   init_pit(); // starts the Programable intervall timer
+   os.init(); // clear the screen and write "Hello World!"
+   init_pit(); // starts the programmable interval timer
  
 
- // these functions connect the interupts handlers to the interupt descriptor table
+ // these functions register the interrupt handlers in the interrupt descriptor table
  UiAOS::CPU::ISR::register_interrupt_handler(3,[](UiAOS::CPU::ISR::registers_t* regs, void* context){ 
     auto* os = (IJI_OS*)context;
     os->interrupt_handler_3(*regs);
@@ -34,17 +34,17 @@ void kernel_main()
     os->interrupt_handler_1(*regs);
  }, (void*)&os);
 
-asm volatile("sti"); // this enables interupts allowing pit and the keyboard function below to 
- UiAOS::IO::Keyboard::hook_keyboard([](uint8_t scancode, void* context){ // this function is triggered when we push a button on the keyboard
+asm volatile("sti"); // This enables interrupts, allowing pit and the keyboard function
+ UiAOS::IO::Keyboard::hook_keyboard([](uint8_t scancode, void* context){ // This function is triggered when we push a button on the keyboard
     auto* os = (IJI_OS*)context;
-    os->write_char(UiAOS::IO::Keyboard::scancode_to_ascii(scancode)); // this writes the character from the keyboard
+    os->write_char(UiAOS::IO::Keyboard::scancode_to_ascii(scancode)); // This writes the character from the keyboard
     },&os); 
 
-// this is to show that our interupt handlers work
+// We trigger our interrupts to show that our interrupt handlers work
 asm volatile ("int $0x03");
 asm volatile ("int $0x02");
 asm volatile ("int $0x01");
-start_sleep_busy((uint32_t) 10000); // this gives you time to see that booth our timer and our inerupt handler works
+start_sleep_busy((uint32_t) 10000); // this line demonstrates functionality of sleep call
 
 
 // below we assign some memory to show that our memory manager works
@@ -54,16 +54,12 @@ void* mem1 = malloc(12);
  void* mem2 = malloc(1200);
  void* mem3 = malloc(12);
 os.print_memory_layout();
-start_sleep_busy((uint32_t) 10000); // time to see the memory before we free som to show that it works
+start_sleep_busy((uint32_t) 10000);
  free(mem2);
  void* mem4 = malloc(12);
  int c = 2;
 
-os.print_memory_layout();
-sleep_interrupt(10000); // again sleep to show that is works
-
-
-
-
+os.print_memory_layout(); //Display functioning paging
+sleep_interrupt(10000); // Sleep with enabled interrupts to show that it works
  
 }
