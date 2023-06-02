3c6073aaba71bb45f632442ca3b09a8649b97b59
Isak Killingrød <isakk@uia.no>
2023-06-02 18:23:00 +0200

    Spell-checked comments on GDT folder

--- group_42/src/GDT/isr.cpp
+++ group_42/src/GDT/isr.cpp
@@ -7,36 +7,36 @@
 
 UiAOS::CPU::ISR::interrupt_t interrupt_handlers[256];
 
-extern "C"{ // gjør en rekke funkjsoner tilgjenglig i assambly
+extern "C"{ // Makes the functions below available to be called in assembly
     void init_isr() asm("init_isr");
     void irq_handler(UiAOS::CPU::ISR::registers_t regs) asm("irq_handler");
     void isr_handler(UiAOS::CPU::ISR::registers_t regs) asm("isr_handler");
 }
 
-void init_isr(){ // setter opp Interupt servise rutines
+void init_isr(){ // Initializes interrupt service routines
     
-    memset(&interrupt_handlers, 0, sizeof(UiAOS::CPU::ISR::isr_t)*256); // sets the adress of all interupthandles to be zero
+    memset(&interrupt_handlers, 0, sizeof(UiAOS::CPU::ISR::isr_t)*256); // sets the address of all interrupt handles to be zero
 }
 
 
-void UiAOS::CPU::ISR::register_interrupt_handler(uint8_t n, isr_t handler, void* context) // conects the given interupt handler to the given interupt
+void UiAOS::CPU::ISR::register_interrupt_handler(uint8_t n, isr_t handler, void* context) // connects the given interrupt handler to the given interrupt
 {
     interrupt_handlers[n].handler = handler; 
     interrupt_handlers[n].context = context;
 }
 
 
-void isr_handler(UiAOS::CPU::ISR::registers_t regs) // this function is trigered when an interupt is triggerd
+void isr_handler(UiAOS::CPU::ISR::registers_t regs) // this function is triggered when an interrupt is triggerd
 {
    
-    uint8_t int_no = regs.int_no & 0xFF; // important line  for extetions ressons
-    UiAOS::CPU::ISR::interrupt_t intrpt = interrupt_handlers[int_no];  // the interupt is retrived from a array
+    uint8_t int_no = regs.int_no & 0xFF; // important line for extention reasons
+    UiAOS::CPU::ISR::interrupt_t intrpt = interrupt_handlers[int_no];  // the interrupt is retrived from a array
     if (intrpt.handler != 0) // if there is a defined handler use the handler
     {
        
         intrpt.handler(&regs, intrpt.context);
     }
-    else // if there is no handler inform on the screen what has happend and enter an endless loop
+    else // if there is no handler write an error message to the screen and enter an endless loop
     {
       
         write_string(0x0B, "Goodbye");
@@ -45,7 +45,7 @@ void isr_handler(UiAOS::CPU::ISR::registers_t regs) // this function is trigered
 }
 
 
-void irq_handler(UiAOS::CPU::ISR::registers_t regs) // this function is triggered when an inerupt occors
+void irq_handler(UiAOS::CPU::ISR::registers_t regs) // this function is triggered when a hardware interrupt occurs
 {
     
     if (regs.int_no >= 40)
